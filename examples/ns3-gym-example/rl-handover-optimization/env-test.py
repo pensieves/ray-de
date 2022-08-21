197e48c66396354a1258ed9c974b10c13457d6d0
Rituraj Joshi <psycic03@gmail.com>
2022-08-22 04:53:57 +0530

    Added starting code of ns3gym environment creation for RL based handover optimization

---
+++ examples/ns3-gym-example/rl-handover-optimization/env-test.py
@@ -0,0 +1,19 @@
+from sys import argv
+from ns3gym import ns3env
+
+debug = bool(int(argv[1]))
+env = ns3env.Ns3Env(debug=debug)
+obs_space = env.observation_space
+act_space = env.action_space
+print(obs_space)
+print(act_space)
+
+s = env.reset()
+print(s)
+
+done = False
+while not done:
+    action = env.action_space.sample()
+    obs, reward, done, info = env.step(action)
+    print(action, obs, reward, done, info)
+print("DONE.")
